a01aca2376555f21c3ed539c9fc81f53f6ed8e5b
Cielesis - Allan COTE <ctn.email.pro@gmail.com>
2021-11-03 09:03:55 +0100

    commit

---
+++ src/pages/DetailsProducts.js
@@ -0,0 +1,10 @@
+import '../styles/detailsproducts.scss'
+
+const DetailsProducts = () => {
+    return (
+        <div className="app-detailsproducts">
+            <p>Details des produits</p>
+        </div>
+    )
+}
+export default DetailsProducts;
