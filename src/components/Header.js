892436b1a7a55d2cfdf0bc153c826fab054a8519
Cielesis - Allan COTE <ctn.email.pro@gmail.com>
2021-11-04 09:08:50 +0100

    v.0.2

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import "../styles/header.scss";
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import Logo from "./Logo";
 
 const Header = () => {
   const [api, setApi] = useState([]);
@@ -19,37 +20,7 @@ const Header = () => {
   return (
     <header className="app-header">
       <div className="app-header-row">
-        <Link className="app-header-logo" to="/">
-          <svg
-            id="Calque_1"
-            data-name="Calque 1"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 1744 488.06"
-          >
-            <defs></defs>
-            <text class="cls-1" transform="translate(350.75 397.78)">
-              DRONIUS
-            </text>
-            <line class="cls-2" x1="387" y1="322.55" x2="107" y2="322.55" />
-            <line class="cls-2" x1="119.5" y1="328.55" x2="119.5" y2="189.55" />
-            <line class="cls-2" y1="189.55" x2="239" y2="189.55" />
-            <line
-              class="cls-2"
-              x1="1357.01"
-              y1="322.55"
-              x2="1637.01"
-              y2="322.55"
-            />
-            <line
-              class="cls-2"
-              x1="1624.51"
-              y1="328.55"
-              x2="1624.51"
-              y2="189.55"
-            />
-            <line class="cls-2" x1="1505" y1="189.55" x2="1744" y2="189.55" />
-          </svg>
-        </Link>
+        <Logo />
         <div className="app-header-cart">
           <button class="snipcart-checkout">
             <span class="snipcart-items-count"></span>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,26 +12,31 @@ const Home = () => {
       </div>
       <div className="app-home-about">
         <div className="app-home-about-section">
-          <img src={about_1}></img>
-          <h2>Qui sommes-nous ?</h2>
-          <p>
-            C'est l'histoire d'une rencontre de 3 jeunes étudiants passionnés.
-            Durant leurs études en électroniques, ils ont fabriqué des drones
-            pour s'amuser. C'est ainsi qu'est né Dronius !
-          </p>
+          <img src={about_1} alt="Illustration étudiants"></img>
+          <div className="app-home-about-text">
+            <h2>Qui sommes-nous ?</h2>
+            <p>
+              C'est l'histoire d'une rencontre de 3 jeunes étudiants passionnés.
+              Durant leurs études en électroniques, ils ont fabriqué des drones
+              pour s'amuser. C'est ainsi qu'est né Dronius !
+            </p>
+          </div>
         </div>
-        <div className="app-home-about-section">
-          <img src={about_2}></img>
-          <h2>Pourquoi Dronius</h2>
-          <p>
-            En achetant nos produits, vous avez la garantie d'obtenir des drones
-            hauts de gamme autant de par leur qualité de finition que par leurs
-            performances dans le temps. De plus, vous obtenez une garantie
-            complète de 5 ans sans conditions et tous les éventuels frais de
-            réparation sont pris en charges.
-          </p>
+        <div className="app-home-about-section reverse">
+          <img src={about_2} alt="Illustration controle qualité"></img>
+          <div className="app-home-about-text">
+            <h2>Pourquoi Dronius ?</h2>
+            <p>
+              En achetant nos produits, vous avez la garantie d'obtenir des
+              drones hauts de gamme autant de par leur qualité de finition que
+              par leurs performances dans le temps. De plus, vous obtenez une
+              garantie complète de 5 ans sans conditions et tous les éventuels
+              frais de réparation sont pris en charges.
+            </p>
+          </div>
         </div>
       </div>
+      <div className="app-home-articles"></div>
     </div>
   );
 };
